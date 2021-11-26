3213370a1533d1e78f24bf63b5af6f325b3b1323
yh56789 <yh567890@gmail.com>
2021-11-26 10:22:36 +0800

    Update websockify.sh

--- conf.d/websockify.sh
+++ conf.d/websockify.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
+cd /root
+su root -l -c 'vncserver :2000 ' 
 websockify --web /usr/share/novnc $PORT localhost:5900
-
-
-
-
-
-
-RUN echo 'cd /root' >>/luo.sh
-RUN echo "su root -l -c 'vncserver :2000 ' "  >>/luo.sh
-RUN echo 'cd /noVNC-1.2.0' >>/luo.sh
-RUN echo './utils/launch.sh  --vnc localhost:7900 --listen 80 ' >>/luo.sh
